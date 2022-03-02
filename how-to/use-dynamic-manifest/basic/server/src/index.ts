ca1fe88b5ce195fc4b12b1b9e0251ea278c245c3
PJ Singh <pj.singh@openfin.co>
2022-03-02 13:12:34 -0500

    Added opening of dfferent manifests

---
+++ how-to/use-dynamic-manifest/basic/server/src/index.ts
@@ -0,0 +1,114 @@
+import *  as express from "express";
+import * as path from "path";
+const app = express();
+//app.use(express.static(path.join(__dirname, '../../public')));
+const port = 8080;
+
+app.get('/', (req, res) => {
+   const env = req.query.env;
+   if(env === 'dev'){
+        res.json({
+        "runtime": {
+            "arguments": "--v=1 --inspect",
+            "version": "stable"
+        },
+        "platform": {
+            "uuid": "how-to-use-dynamic-manifest",
+            "autoShow": true,
+            "icon":"http://localhost:8080/favicon.ico",
+            "preloadScripts": [
+            ],
+            "defaultWindowOptions": {
+                "preloadScripts": [
+                
+                ]
+            },
+            "defaultViewOptions": {
+                "preloadScripts": [
+                
+                ]
+            }
+        },
+        "snapshot": {
+            "windows": [
+            {
+                "layout": {
+                "content": [
+                    {
+                    "type": "stack",
+                    "id": "no-drop-target",
+                    "content": [
+                        {
+                        "type": "component",
+                        "componentName": "view",
+                        "componentState": {
+                            "processAffinity": "ps_1",
+                            "url": "http://google.com"
+                        }
+                        }
+                    ]
+                    }
+                ]
+                }
+            }
+            ]
+        }
+        });
+   }else{
+    res.json({
+        "runtime": {
+            "arguments": "--v=1 --inspect",
+            "version": "stable"
+        },
+        "platform": {
+            "uuid": "how-to-use-dynamic-manifest",
+            "autoShow": true,
+            "icon":"http://localhost:8080/favicon.ico",
+            "preloadScripts": [
+            ],
+            "defaultWindowOptions": {
+                "preloadScripts": [
+                
+                ]
+            },
+            "defaultViewOptions": {
+                "preloadScripts": [
+                
+                ]
+            }
+        },
+        "snapshot": {
+            "windows": [
+            {
+                "layout": {
+                "content": [
+                    {
+                    "type": "stack",
+                    "id": "no-drop-target",
+                    "content": [
+                        {
+                        "type": "component",
+                        "componentName": "view",
+                        "componentState": {
+                            "processAffinity": "ps_1",
+                            "url": "http://localhost:8080/html/app.html"
+                        }
+                        }
+                    ]
+                    }
+                ]
+                }
+            }
+            ]
+        }
+        });
+   }
+})
+
+app.get('/html/app.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../public/html/app.html'));
+});
+
+app.listen(port, () => {
+    console.log("server is listening on port", port);
+});
