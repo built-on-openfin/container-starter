5a0a05e9cdba424346759d5a17a2943f47bd85ce
PJ Singh <pj.singh@openfin.co>
2022-04-06 10:37:06 -0400

    Updated as per pull request

--- how-to/create-window/client/src/app.ts
+++ how-to/create-window/client/src/app.ts
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-async function openDynamicWindow() {
+async function openDynamicApplicationWindow() {
     const winOption = {
         name: 'child',
         defaultWidth: 800,
@@ -20,17 +20,17 @@ async function openDynamicWindow() {
     return await fin.Window.create(winOption);
 }
 
-async function openManifestWindow() {
-    fin.Application.startFromManifest('http://localhost:8080/config/manifest.json')
+async function openManifestApplicationWindow() {
+    fin.Application.startFromManifest('http://localhost:8080/config/manifest.fin.json')
         .then(app => console.log('App is running'))
         .catch(err => console.log(err));
 }
 
 async function init(): Promise<void> {
-    let btn = document.getElementById("btn-open-dynamic-window");
-    btn.addEventListener("click", (e: Event) => openDynamicWindow());
-    let btn1 = document.getElementById("btn-open-manifest-window");
-    btn1.addEventListener("click", (e: Event) => openManifestWindow());
+    const btn = document.getElementById("btn-open-dynamic-window");
+    btn.addEventListener("click", (e: Event) => openDynamicApplicationWindow());
+    const btn1 = document.getElementById("btn-open-manifest-window");
+    btn1.addEventListener("click", (e: Event) => openManifestApplicationWindow());
 }
 
 
