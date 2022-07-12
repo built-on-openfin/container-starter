b8d68ed2d6b59a49cbbd1db2dd752aa1673be2f2
adam-saland <adam.saland@openfin.co>
2022-07-12 11:52:33 -0400

    remove unused code / css

--- how-to/use-interop/setup-multi-platform-interop/client/webpack.config.js
+++ how-to/use-interop/setup-multi-platform-interop/client/webpack.config.js
@@ -22,26 +22,6 @@ module.exports = [
 		}
 	},
 	{
-		entry: './client/src/view.ts',
-		devtool: 'inline-source-map',
-		module: {
-			rules: [
-				{
-					test: /\.tsx?$/,
-					use: 'ts-loader',
-					exclude: /node_modules/
-				}
-			]
-		},
-		resolve: {
-			extensions: ['.tsx', '.ts', '.js']
-		},
-		output: {
-			filename: 'view.bundle.js',
-			path: path.resolve(__dirname, '..', 'public', 'js')
-		}
-	},
-	{
 		entry: './client/src/window.ts',
 		devtool: 'inline-source-map',
 		module: {
