3e791bf3931bb572876de5118b39a649952e6115
Martyn Janes <martyn@obany.com>
2023-06-02 08:57:34 +0200

    Expand example

--- how-to/use-content-creation-rules/client/src/app.ts
+++ how-to/use-content-creation-rules/client/src/app.ts
@@ -1,10 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const openGoogleBtn = document.querySelector("#open-google");
-	openGoogleBtn.addEventListener("click", (e) => {
+document.addEventListener("DOMContentLoaded", async () => {
+	const openView = document.querySelector("#open-view");
+	openView.addEventListener("click", (e) => {
 		window.open("https://www.google.com");
 	});
-	const openCdnBtn = document.querySelector("#open-cdn");
-	openCdnBtn.addEventListener("click", (e) => {
-		window.open("https://cdn.openfin.co/embed-web/chart.html");
+
+	const openViewTarget = document.querySelector("#open-view-target");
+	const viewTargets = [
+		"https://www.examples.com/category/business/advertising",
+		"https://www.examples.com/category/business/agenda",
+		"https://www.examples.com/category/business/agreement"
+	];
+
+	let viewTargetIndex = 0;
+	openViewTarget.addEventListener("click", (e) => {
+		window.open(viewTargets[viewTargetIndex++ % viewTargets.length], "examples");
+	});
+
+	const openWindow = document.querySelector("#open-window");
+	openWindow.addEventListener("click", (e) => {
+		window.open("https://www.bing.com");
+	});
+
+	const openWindowTarget = document.querySelector("#open-window-target");
+	const windowTargets = [
+		"https://www.examples.com/category/education/case-study",
+		"https://www.examples.com/category/education/essays",
+		"https://www.examples.com/category/education/finance"
+	];
+
+	let windowTargetIndex = 0;
+	openWindowTarget.addEventListener("click", (e) => {
+		window.open(windowTargets[windowTargetIndex++ % windowTargets.length], "examples2");
+	});
+
+	const openBrowser = document.querySelector("#open-browser");
+	openBrowser.addEventListener("click", (e) => {
+		window.open("https://www.microsoft.com");
+	});
+
+	const openBlocked = document.querySelector("#open-blocked");
+	openBlocked.addEventListener("click", (e) => {
+		window.open("https://www.apple.com");
 	});
 });
--- how-to/use-content-creation-rules/client/src/default-platform-window.ts
+++ how-to/use-content-creation-rules/client/src/default-platform-window.ts
@@ -1,3 +1,5 @@
+import type OpenFin from "@openfin/core";
+
 window.addEventListener("DOMContentLoaded", async () => {
 	const platform = fin.Platform.getCurrentSync();
 	const me = fin.me as OpenFin.Window;
@@ -26,19 +28,80 @@ window.addEventListener("DOMContentLoaded", async () => {
 
 		return me.restore();
 	}
-	await me.on("child-view-created", async (e) => {
-		const viewOptions: OpenFin.PlatformViewCreationOptions = {
-			url: e.childOptions.url,
-			target: fin.me.identity
-		};
-		await platform.createView(viewOptions, fin.me.identity);
-	});
 
 	await me.on("view-child-view-created", async (e) => {
-		const viewOptions: OpenFin.PlatformViewCreationOptions = {
-			url: e.childOptions.url,
-			target: fin.me.identity
-		};
-		await platform.createView(viewOptions, fin.me.identity);
+		// The content creation rules construct the options for the view in the childOptions
+		// The view has been created but not yet attached or navigated
+		console.log(e);
+		const viewOptions: OpenFin.PlatformViewCreationOptions = e.childOptions;
+		let originalTargetName;
+		let create = true;
+
+		// If the window.open contained a target it is the viewOptions.name,
+		// the name starts internal-generated if a target wasn't specified
+		if (!viewOptions.name.startsWith("internal-generated")) {
+			// This is a window.open with a target, but we can't use the original target name
+			// as the view name, because this would not allow it to be re-used, so we
+			// substitute a derived view name and then remove the old view later
+
+			// Store the original view name so that we can destroy it later
+			originalTargetName = viewOptions.name;
+
+			// Set a new derived name for the view based on the original name
+			viewOptions.name = `${viewOptions.name}-view`;
+
+			try {
+				// See if we can get the derived reusable view name
+				const currentView = fin.View.wrapSync({ uuid: fin.me.identity.uuid, name: viewOptions.name });
+
+				// If we have it then navigate to the requested url
+				await currentView.navigate(viewOptions.url);
+
+				// And focus it, which will switch to the tab if its not active
+				await currentView.focus();
+
+				// We have reused so no need to create
+				create = false;
+			} catch {
+				// Something failed, most likely the view does not exist
+				// so the create flag will still be set
+			}
+		}
+
+		// The create flag is set because we couldn't find the existing view
+		if (create) {
+			// Create the view using the original view options, this will just attach the
+			// view created for us by the content creation rules
+			// Or if it had the name swapped due to a target specified in the original
+			// window.open it will create a new view with the derived name
+			const view = await platform.createView(viewOptions, e.target as OpenFin.Identity);
+
+			// We must explicitly navigate the view created by the content creation rules
+			await view.navigate(viewOptions.url);
+		}
+
+		// Cleanup the view that was created with the original target name if it had one
+		// as we have substituted a view with a derived name
+		if (originalTargetName) {
+			try {
+				const targetView = fin.View.wrapSync({ uuid: fin.me.identity.uuid, name: originalTargetName });
+				await targetView.destroy();
+			} catch {}
+		}
+	});
+
+	await me.on("view-child-window-created", async (e) => {
+		// Called when content is opened in a window
+		console.log(e);
+	});
+
+	await me.on("view-child-content-opened-in-browser", async (e) => {
+		// Called when content is opened in the browser
+		console.log(e);
+	});
+
+	await me.on("view-child-content-blocked", async (e) => {
+		// Called when content is blocked
+		console.log(e);
 	});
 });
