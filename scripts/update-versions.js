479ae3d7f1f52f7447512e7388b189a8346ae395
adam-saland <adam.saland@openfin.co>
2023-10-10 11:25:40 -0400

    fix script and bump core version

--- scripts/update-versions.js
+++ scripts/update-versions.js
@@ -34,7 +34,7 @@ for (const fileNamePattern of fileNamePatterns) {
 				json[targetKey]['openfin-adapter'] = replacementValue;
 			}
 
-			if (targetKey === 'dependencies' && json[targetKey] !== undefined) {
+			if ((targetKey === 'dependencies' || targetKey === 'devDependencies') && json[targetKey] !== undefined) {
 				json[targetKey]['@openfin/core'] = replacementValue;
 			}
 
