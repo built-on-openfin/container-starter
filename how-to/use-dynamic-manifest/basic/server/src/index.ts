966f6e30a01a72f3a04386537237c6bb5609a91c
Joe Ransegnola <joe.ransegnola@openfin.co>
2024-02-16 10:46:49 -0600

    updating logic and adding backup manifest for win7

--- how-to/use-dynamic-manifest/basic/server/src/index.ts
+++ how-to/use-dynamic-manifest/basic/server/src/index.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import os from "node:os";
+import { platform } from "node:process";
 import path from "path";
-import { platform } from 'node:process';
-import os from 'node:os';
 
 const app = express();
-
 const mainPath = path.join(__dirname, "..", "..", "public");
-const macManifest = require(mainPath + '/mac.manifest.fin.json');
-const windowsManifest = require(mainPath + '/win.manifest.fin.json');
 const port = 5050;
 
 console.log(`Main Path / = ${mainPath}`);
@@ -15,20 +12,25 @@ console.log(`Main Path / = ${mainPath}`);
 app.get("/manifest", (req, res) => {
 	const env = req.query.env;
 	const plat = platform;
-	console.log(`Your platform is: ${plat}`);  // Gives you the overall Platform from node (Win32, Darwin for Mac, etc..)
-	console.log(`Your platform is: ${os.type()}`); // Gives you the System type, which could be: Windows_NT, Cygwin, Ming, etc..
+	console.log(`Your platform is: ${plat}`); // Gives you the overall Platform from node (Win32, Darwin for Mac, etc..)
+	console.log(`Your OS is: ${os.type()}`); // Gives you the System type, which could be: Windows_NT, Cygwin, Ming, etc..
 	console.log(`Your version is: ${os.release()}`); // Gives you the actual release # for the platform (i.e. 10.0.22621)
 
-	if (plat && plat === 'darwin') {
+	if (plat && plat === "darwin") {
 		// Launch Mac OS Manifest
-		res.json(macManifest);
+		res.sendFile(path.join(__dirname, "../../public/mac.manifest.fin.json"));
 	}
 
-	if (plat && plat === 'win32') {
-		// Launch Windows Manifest.
-		res.json(windowsManifest);
+	if (plat && plat === "win32") {
+		// Launch a Windows Manifest.
+		if (os.type() === "Windows_NT") {
+			// Windows 10/11
+			res.sendFile(path.join(__dirname, "../../public/win.manifest.fin.json"));
+		} else {
+			// Windows 7 and other versions.
+			res.sendFile(path.join(__dirname, "../../public/win7.manifest.fin.json"));
+		}
 	}
-
 });
 
 app.get("/html/app-dev.html", (req, res) => {
