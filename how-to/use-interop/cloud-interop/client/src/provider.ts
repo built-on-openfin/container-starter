f585467805c7e5326f32e86157c09863de58b2d8
Johan Sandersson <98814236+openfin-johans@users.noreply.github.com>
2024-12-02 16:24:31 +0000

    Update provider.ts
    Adding workaround for connect params

--- how-to/use-interop/cloud-interop/client/src/provider.ts
+++ how-to/use-interop/cloud-interop/client/src/provider.ts
@@ -1,4 +1,4 @@
-import { cloudInteropOverride } from "@openfin/cloud-interop";
+import { cloudInteropOverride,CloudInteropOverrideParams } from "@openfin/cloud-interop";
 import type OpenFin from "@openfin/core";
 import { getManifestCustomSettings } from "./settings";
 
@@ -6,9 +6,40 @@ window.addEventListener("DOMContentLoaded", async () => {
 	const customSettings = await getManifestCustomSettings();
 	const interopOverrides = [];
 
+	// TEMP WORKAROUND TO CREATE CONNECT PARAMS
+
+	const jwtConnectParams:CloudInteropOverrideParams = {
+		url: "<PLEASE ASK OPENFIN FOR A URL>",
+		authenticationType: "jwt",
+  		jwtAuthenticationParameters: {
+    		authenticationId: "<PLEASE ASK OPENFIN FOR THE AUTH ID>",
+    		jwtRequestCallback: () => "<PLEASE ASK OPENFIN FOR JWT DETAILS TO CREATE TOKEN>"
+  		},
+		platformId: customSettings?.cloudInteropProvider?.connectParams?.platformId as string,
+		sourceId: customSettings?.cloudInteropProvider?.connectParams?.sourceId,
+		sourceDisplayName: customSettings?.cloudInteropProvider?.connectParams?.sourceDisplayName
+	}
+
+	const basicConnectParams:CloudInteropOverrideParams = {
+		url: "<PLEASE ASK OPENFIN FOR A URL>",
+		authenticationType: "basic",
+		basicAuthenticationParameters: {
+			username: "<PLEASE ASK OPENFIN FOR A USER ID>",
+			password: "<PLEASE ASK OPENFIN FOR A PASSWORD>"
+		},
+		platformId: customSettings?.cloudInteropProvider?.connectParams?.platformId as string,
+		sourceId: customSettings?.cloudInteropProvider?.connectParams?.sourceId,
+		sourceDisplayName: customSettings?.cloudInteropProvider?.connectParams?.sourceDisplayName
+	}
+
+	// END 
+
 	if (customSettings?.cloudInteropProvider?.enabled) {
 		const initializedCloudInteropOverride = (await cloudInteropOverride(
-			customSettings?.cloudInteropProvider?.connectParams
+		// TEMP WORKAROUND
+		//customSettings?.cloudInteropProvider?.connectParams
+		//basicConnectParams
+		jwtConnectParams
 		)) as unknown as OpenFin.ConstructorOverride<OpenFin.InteropBroker>;
 		interopOverrides.push(initializedCloudInteropOverride);
 	}
