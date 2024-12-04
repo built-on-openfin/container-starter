35f9d94c44586f9cd84efb04c0f33e35e8c13e7e
Joe Ransegnola <joe.ransegnola@openfin.co>
2024-12-04 16:39:29 -0600

    initial attempt at adding usb demo

---
+++ how-to/detect-usb-devices/README.md
@@ -0,0 +1,45 @@
+# Detect USB Devices
+
+## How it Works
+
+The app.ts listens for button clicks and creates with a dynamic openfin window or from a manifest.
+
+The manifest is located in the config folder along with the manifest for this sample application
+
+## Get Started
+
+Follow the instructions below to get up and running.
+
+### Set up the project
+
+1. Install dependencies and do the initial build. Note that these examples assume you are in the sub-directory for the example.
+
+```shell
+npm run setup
+```
+
+2. Build the project.
+
+```shell
+npm run build
+```
+
+3. Start the test server in a new window.
+
+```shell
+npm run start
+```
+
+4. Start the Platform application.
+
+```shell
+npm run client
+```
+
+### What you will see
+
+1. The default platform provider window (as the manifest sets autoshow to true to help with development)
+
+## A note about this example
+
+This is an example of how to use OpenFin APIs to configure OpenFin Container. Its purpose is to provide an example and suggestions. **DO NOT** assume that it contains production-ready code. Please use this as a guide and provide feedback. Thanks!
---
+++ how-to/detect-usb-devices/client/src/app.ts
@@ -0,0 +1,84 @@
+import type OpenFin from "@openfin/core";
+
+document.addEventListener("DOMContentLoaded", async () => {
+	try {
+		await initDom();
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+/**
+ * Initialize the DOM components.
+ */
+async function initDom(): Promise<void> {
+	const btnOpenDynamicWindow = document.querySelector("#btn-open-dynamic-window");
+	if (btnOpenDynamicWindow) {
+		btnOpenDynamicWindow.addEventListener("click", async (e: Event) => openDynamicApplicationWindow());
+	}
+
+	const btnCheckForDevices = document.querySelector("#btn-check-for-usb-devices");
+	if (btnCheckForDevices) {
+		btnCheckForDevices.addEventListener("click", checkForUsbDevices);
+	}
+}
+
+/**
+ * Checks to see what devices to connect.
+ */
+async function requestDevice(): Promise<void> {
+	await navigator.usb.requestDevice({ filters: [{ vendorId: 0x1133 }] });
+}
+
+
+/**
+ * Open a window using dynamic options.
+ * @returns The window.
+ */
+async function openDynamicApplicationWindow(): Promise<OpenFin.Window> {
+	const winOption = {
+		name: "child",
+		defaultWidth: 800,
+		defaultHeight: 800,
+		url: "https://developers.openfin.co/of-docs/docs/connect-usb-hid#find-all-connected-devices",
+		frame: true,
+		autoShow: true
+	};
+	return fin.Window.create(winOption);
+}
+
+/**
+ * Use the usb.getDevices method to get a list of connected devices.
+ */
+async function checkForUsbDevices(): Promise<void> {
+	await requestDevice();
+	// Get all connected USB devices the website has been granted access to.
+	// if ("usb" in navigator) {
+	// 	const usb = navigator.usb;
+	// 	await usb.getDevices()
+	// 	.then((devices) => {
+	// 		if (devices.length > 0) {
+	// 			console.log(`There are ${devices.length} detected USB devices.`);
+	// 			for (const device of devices) {
+	// 				console.log(device.productName); // "Arduino Micro"
+	// 				console.log(device.manufacturerName); // "Arduino LLC"
+	// 			}
+	// 		} else {
+	// 			console.log("There are no detected USB devices.");
+	// 		}
+	// 		return true;
+	// 	})
+	// 	.catch((error) => {
+	// 		console.error(error);
+	// 	});
+	// } else {
+	// 	console.log("There is no USB detection on the Navigator window object for this browser.");
+	// }
+}
+
+/**
+ *
+ */
+// function addDeviceToList() {
+	
+// }
