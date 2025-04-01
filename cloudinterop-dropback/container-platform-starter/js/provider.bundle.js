56523bc662ed9bef6b2e8850c5876235968465a0
joeransegnola <129192453+joeransegnola@users.noreply.github.com>
2025-04-01 14:08:11 +0000

    Deploying to gh-pages from @ built-on-openfin/container-starter@8f088a80f8d186f6039e829ef433d6a18c7cc7a5 🚀

---
+++ cloudinterop-dropback/container-platform-starter/js/provider.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************************!*\
+  !*** ./client/src/provider.ts ***!
+  \********************************/
+
+fin.Platform.init({}).catch((error) => console.error(error));
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJvdmlkZXIuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEdBQUcsQ0FBQyxRQUFRLENBQUMsSUFBSSxDQUFDLEVBQUUsQ0FBQyxDQUFDLEtBQUssQ0FBQyxDQUFDLEtBQUssRUFBRSxFQUFFLENBQUMsT0FBTyxDQUFDLEtBQUssQ0FBQyxLQUFLLENBQUMsQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY29udGFpbmVyLXBsYXRmb3JtLXN0YXJ0ZXIvLi9jbGllbnQvc3JjL3Byb3ZpZGVyLnRzIl0sInNvdXJjZXNDb250ZW50IjpbImZpbi5QbGF0Zm9ybS5pbml0KHt9KS5jYXRjaCgoZXJyb3IpID0+IGNvbnNvbGUuZXJyb3IoZXJyb3IpKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
