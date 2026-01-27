707daa4d472a72aff0f003ae3cbd052485e105e9
NicholasNewman <nicholas.newman90@gmail.com>
2026-01-27 16:08:03 +0000

    Deploying to gh-pages from @ built-on-openfin/container-starter@89388ce3b64ec013a70d17891579512fbccaf440 🚀

---
+++ main/use-print/js/provider.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************************!*\
+  !*** ./client/src/provider.ts ***!
+  \********************************/
+
+fin.Platform.init().catch((error) => console.error(error));
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJvdmlkZXIuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEdBQUcsQ0FBQyxRQUFRLENBQUMsSUFBSSxFQUFFLENBQUMsS0FBSyxDQUFDLENBQUMsS0FBSyxFQUFFLEVBQUUsQ0FBQyxPQUFPLENBQUMsS0FBSyxDQUFDLEtBQUssQ0FBQyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly91c2UtcHJpbnQvLi9jbGllbnQvc3JjL3Byb3ZpZGVyLnRzIl0sInNvdXJjZXNDb250ZW50IjpbImZpbi5QbGF0Zm9ybS5pbml0KCkuY2F0Y2goKGVycm9yKSA9PiBjb25zb2xlLmVycm9yKGVycm9yKSk7XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9
